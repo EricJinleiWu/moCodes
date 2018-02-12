5d7d243131477fc0ebf0a5ec79717cbaddd31965
EricJinleiWu <wujl_0351@163.com>
2018-02-12 18:10:52 +0800

    define API for moCloudClientLib, init tstfiles for it.

---
+++ moProject/moCloud/moCloudClientLib/tst/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "moLogger.h"
+#include "moUtils.h"
+#include "moCrypt.h"
+
+#include "moCloudUtilsTypes.h"
+#include "moCloudUtilsCheck.h"
+#include "moCloudUtilsCrypt.h"
+#include "moCloudClientLib.h"
+
+int main(int arc, char **argv)
+{
+    return 0;
+}
